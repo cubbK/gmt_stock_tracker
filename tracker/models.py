3935288360aa41de00212392ad68f8c06e9a0d42
Dan <yo@gmail.om>
2025-02-28 15:43:31 +0100

    add game class

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class GmtGameId(models.Model):
+    game_id = models.CharField(
+        max_length=255,
+        verbose_name="Bradspelpriset Game ID",
+        help_text="get it from bradspelspriser.se",
+    )
+    game_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.game_name
